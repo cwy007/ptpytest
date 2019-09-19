d991fc763f839a5bc36e8dc38390f4896827e03b
chenweiyan <chanweiyan007@gmail.com>
2019-09-19 16:33:27 +0800

    update README

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # 《Pytest 测试实战》
 
 书中源码
+
+## 添加虚拟换环境
+
+```bash
+
+pip3 install -U virtualenv
+python3 -m virtualenv env
+source venv/bin/activate
+pip install pytest
+
+```
+
+## 在终端启动虚拟环境
+
+```bash
+
+source venv/bin/activate
+
+```
--- code/ch1/tasks/test_three.py
+++ code/ch1/tasks/test_three.py
@@ -5,6 +5,8 @@ from collections import namedtuple
 Task = namedtuple('Task', ['summary', 'owner', 'done', 'id'])
 Task.__new__.__defaults__ = (None, None, False, None)
 
+print(Task)
+
 
 def test_defaults():
     """Using no parameters should invoke defaults."""
